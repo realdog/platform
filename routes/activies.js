dfdd3446672ea7a9f182ea8e2916134b7315e457
realdog <tpercila@gmail.com>
2014-01-29 01:04:22 +0800

    1

--- routes/activies.js
+++ routes/activies.js
@@ -3,7 +3,7 @@ var crypto = require('crypto');
 var fs = require('fs');
 
 exports.participate = function(req, res){
-    if (req.params.gid || req.params.bkey || req.params.msgkey ) {
+    if (req.params.gid && req.params.bkey && req.params.msgkey ) {
         var pem = fs.readFileSync('/users/olddog/key/server.key');
         var key = pem.toString('ascii');
         var decrypted = "";
@@ -11,6 +11,14 @@ exports.participate = function(req, res){
         decrypted = decipher.update(req.params.msgkey, 'hex', 'utf-8');
         decrypted += decipher.final('utf-8');
         console.log(JSON.parse(decrypted.toString()));
+        var password = getUserPasswordByUid();
+        if (password = decrypted.password) {
+            var template = getGameByGid();
+            ejs.render(template, decrypted.param);
+            ejs.display();
+        } else {
+            
+        }
     }
 
     console.log(req.body);
